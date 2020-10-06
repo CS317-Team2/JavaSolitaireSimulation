e4d5cc66ae12631fafa5608d7c2d45785ac50df5
vincentk21 <vincent.kahlhamer@gmail.com>
2020-10-05 23:15:03 -0500

    Set up the setup for the game

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -4,9 +4,53 @@ public class GameBoard {
 	
 	private ArrayList<Card> spadeFoundation, clubFoundation, heartFoundation, diamondFoundation;
 	private ArrayList<Card> tableau1, tableau2, tableau3, tableau4, tableau5, tableau6, tableau7;
-
+	private ArrayList<Card> deck;
+	
 	public GameBoard() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void setUp(CardDeck a) {
+		a.shuffle();
+		tableau1.add(a.drawCard());
+		
+		tableau2.add(a.drawCard());
+		tableau2.add(a.drawCard());
+		
+		tableau3.add(a.drawCard());
+		tableau3.add(a.drawCard());
+		tableau3.add(a.drawCard());
+		
+		tableau4.add(a.drawCard());
+		tableau4.add(a.drawCard());
+		tableau4.add(a.drawCard());
+		tableau4.add(a.drawCard());
+		
+		tableau5.add(a.drawCard());
+		tableau5.add(a.drawCard());
+		tableau5.add(a.drawCard());
+		tableau5.add(a.drawCard());
+		tableau5.add(a.drawCard());
+		
+		tableau6.add(a.drawCard());
+		tableau6.add(a.drawCard());
+		tableau6.add(a.drawCard());
+		tableau6.add(a.drawCard());
+		tableau6.add(a.drawCard());
+		tableau6.add(a.drawCard());
+		
+		tableau7.add(a.drawCard());
+		tableau7.add(a.drawCard());
+		tableau7.add(a.drawCard());
+		tableau7.add(a.drawCard());
+		tableau7.add(a.drawCard());
+		tableau7.add(a.drawCard());
+		tableau7.add(a.drawCard());
+		
+		deck = a.getPlayingDeck();
+		
+	}
+	
+	///IMPLEMENT RULES HERE
+	
 }
